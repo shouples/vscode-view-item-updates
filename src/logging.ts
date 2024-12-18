3250b9560e1daa2ac336f9abe0128d53eb1b2728
Dave Shoup <dave.shoup@gmail.com>
2024-12-18 17:26:59 -0500

    fix output channel logging

--- src/logging.ts
+++ src/logging.ts
@@ -10,31 +10,31 @@ export class Logger {
   trace(message: string, ...args: any[]) {
     const prefix = this.logPrefix("trace");
     console.debug(`${prefix} ${message}`, ...args);
-    OUTPUT_CHANNEL.trace(`[${this.name}] ${message}`);
+    OUTPUT_CHANNEL.trace(`[${this.name}] ${message}`, ...args);
   }
 
   debug(message: string, ...args: any[]) {
     const prefix = this.logPrefix("debug");
     console.debug(`${prefix} ${message}`, ...args);
-    OUTPUT_CHANNEL.debug(`[${this.name}] ${message}`);
+    OUTPUT_CHANNEL.debug(`[${this.name}] ${message}`, ...args);
   }
 
   info(message: string, ...args: any[]) {
     const prefix = this.logPrefix("info");
     console.info(`${prefix} ${message}`, ...args);
-    OUTPUT_CHANNEL.info(`[${this.name}] ${message}`);
+    OUTPUT_CHANNEL.info(`[${this.name}] ${message}`, ...args);
   }
 
   warn(message: string, ...args: any[]) {
     const prefix = this.logPrefix("warn");
     console.warn(`${prefix} ${message}`, ...args);
-    OUTPUT_CHANNEL.warn(`[${this.name}] ${message}`);
+    OUTPUT_CHANNEL.warn(`[${this.name}] ${message}`, ...args);
   }
 
   error(message: string, ...args: any[]) {
     const prefix = this.logPrefix("error");
     console.error(`${prefix} ${message}`, ...args);
-    OUTPUT_CHANNEL.error(`[${this.name}] ${message}`);
+    OUTPUT_CHANNEL.error(`[${this.name}] ${message}`, ...args);
   }
 
   private logPrefix(level: string) {
